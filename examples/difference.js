fccc46cde99493378915b6e3f6ea44290873ec94
Mykolas Juraitis <mykolas@automata.tech>
2022-11-23 12:25:07 +0000

    feat: add translate operation

--- examples/difference.js
+++ examples/difference.js
@@ -1,10 +1,15 @@
+translate({
+  x: 1,
+  y: 0,
+  z: 0,
+},
 difference(
   sphere({
-    radius: 1.5,
+    radius: 0.5,
   }),
   cone({
-    radiusTop: 0.5,
-    radiusBottom: 1,
-    height: 2,
+    radiusTop: 0.25,
+    radiusBottom: 0.5,
+    height: 1,
   })
-);
+));
