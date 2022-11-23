41622742f1495d29923254503c13b54de09be0e2
Mykolas Juraitis <mykolas.juraitis@gmail.com>
2022-11-23 18:10:57 +0000

    feat: support multiple object in the scene

--- examples/difference.js
+++ examples/difference.js
@@ -3,13 +3,31 @@ translate({
   y: 0,
   z: 0,
 },
+  difference(
+    sphere({
+      radius: 0.5,
+    }),
+      cone({
+        radiusTop: 0.25,
+        radiusBottom: 0.5,
+        height: 1,
+      })
+  )
+);
+
 difference(
   sphere({
     radius: 0.5,
   }),
-  cone({
-    radiusTop: 0.25,
-    radiusBottom: 0.5,
-    height: 1,
-  })
-));
+  translate({
+    x: -0.4,
+    y: -0.4,
+    z: -0.4,
+  },
+    box({
+      width: 0.8,
+      height: 0.8,
+      depth: 0.8,
+    })
+  )
+);
