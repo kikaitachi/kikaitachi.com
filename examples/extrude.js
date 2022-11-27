1475495b198034de3563f8573b971d0d8f8f511f
Mykolas Juraitis <mykolas@automata.tech>
2022-11-27 18:39:24 +0000

    docs: link to formulas in extrude example

--- examples/extrude.js
+++ examples/extrude.js
@@ -8,12 +8,14 @@ for (let i = 0; i < k; i++) {
   for (let j = 0.0; j < 360.0 / k; j += angularStep) {
     const angle = (i * 360.0 / k + j) * Math.PI / 180;
     if (i % 2 == 0) {
+      // https://en.wikipedia.org/wiki/Epicycloid
       points.push({
         x: (R + r) * Math.cos(angle) - r * Math.cos((R + r) / r * angle),
         y: 0,
         z: (R + r) * Math.sin(angle) - r * Math.sin((R + r) / r * angle),
       });
     } else {
+      // https://en.wikipedia.org/wiki/Hypocycloid
       points.push({
         x: (R - r) * Math.cos(angle) + r * Math.cos((R - r) / r * angle),
         y: 0,
