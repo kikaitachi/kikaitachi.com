709b05e589d1557a4de9aaf8b7bd58c5a5b5ce25
Mykolas Juraitis <61438920+kikaitachi@users.noreply.github.com>
2023-02-19 15:22:09 +0000

    feat(examples): demonstrate meshing of 2 gears

--- examples/extrude.js
+++ examples/extrude.js
@@ -1,10 +1,10 @@
 const points = [];
-const numberOfTeeth = 15;
-const segmentThickness = 30;
+const numberOfTeeth = 14;
+const segmentThickness = 28.5;
 const k = numberOfTeeth * 2;
 const R = segmentThickness / 2; //segmentThickness / (2 - 2.0 / numberOfTeeth);
 const r = R / k;
-const angularStep = 0.5;
+const angularStep = 2.0;
 const height = 2.5;
 const jointHoleRadius = 2.1;
 const screwHoleRadius = 2.1;
@@ -26,34 +26,39 @@ for (let i = 0; i < scewsPerAxis - 1; i++) {
   }
 }
 
-for (let i = 0; i < k; i++) {
-  for (let j = 0.0; j < 360.0 / k; j += angularStep) {
-    const angle = (i * 360.0 / k + j) * Math.PI / 180;
-    if (i % 2 == 0) {
-      // https://en.wikipedia.org/wiki/Epicycloid
-      points.push({
-        x: (R + r) * Math.cos(angle) - r * Math.cos((R + r) / r * angle),
-        y: (R + r) * Math.sin(angle) - r * Math.sin((R + r) / r * angle),
-        z: 0,
-      });
-    } else {
-      // https://en.wikipedia.org/wiki/Hypocycloid
-      points.push({
-        x: (R - r) * Math.cos(angle) + r * Math.cos((R - r) / r * angle),
-        y: (R - r) * Math.sin(angle) - r * Math.sin((R - r) / r * angle),
-        z: 0,
-      });
+const cycloidalGear = (R, r, k, angularStep) => {
+  const points = [];
+  for (let i = 0; i < k; i++) {
+    for (let j = 0.0; j < 360.0 / k; j += angularStep) {
+      const angle = (i * 360.0 / k + j) * Math.PI / 180;
+      if (i % 2 == 0) {
+        // https://en.wikipedia.org/wiki/Epicycloid
+        points.push({
+          x: (R + r) * Math.cos(angle) - r * Math.cos((R + r) / r * angle),
+          y: (R + r) * Math.sin(angle) - r * Math.sin((R + r) / r * angle),
+          z: 0,
+        });
+      } else {
+        // https://en.wikipedia.org/wiki/Hypocycloid
+        points.push({
+          x: (R - r) * Math.cos(angle) + r * Math.cos((R - r) / r * angle),
+          y: (R - r) * Math.sin(angle) - r * Math.sin((R - r) / r * angle),
+          z: 0,
+        });
+      }
     }
   }
-}
-points.push(points[0]);
+  points.push(points[0]);
 
-const gear = extrude({
-  points: points,
-  x: 0,
-  y: 0,
-  z: height,
-});
+  return extrude({
+    points: points,
+    x: 0,
+    y: 0,
+    z: height,
+  });
+};
+
+const gear = cycloidalGear(R, r, k, angularStep);
 
 difference(
   union(
@@ -68,7 +73,22 @@ difference(
         y: segmentThickness,
         z: height,
       })
-    )
+    ),
+    translate({
+      x: R * 1.5,
+      y: 0,
+      z: 0
+    },
+    /*rotateZ({
+      degrees: 360 / (2 * 3),
+    },
+    cycloidalGear(R / 2, (R / 2) / (2 * 3), 2 * 3, angularStep)
+    ))*/
+    rotateZ({
+      degrees: 360 / (k / 2),
+    },
+    cycloidalGear(R / 2, r, k / 2, angularStep)
+    ))
   ),
   cylinder({
     radius: jointHoleRadius,
